84ea403646b05baa4c394cede4cb15d580dacdc4
youh4ck3dme <h4ck3d@h4ck3d.me>
2026-04-09 19:46:01 +0200

    fix: remove unsupported react-router v7 flag

--- src/App.tsx
+++ src/App.tsx
@@ -99,7 +99,7 @@ const App = () => {
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true, v7_fetcherPersist: true }}>
+          <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
             <InstallPrompt />
             <CookieConsent />
             <AuthProvider>
